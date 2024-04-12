7ae20d049ee1b92490768b83e85fd6d009e90964
yaotianchi <yaotianchi@longcom.com>
2024-04-12 23:31:44 +0800

    init

---
+++ src/main/java/io/martin/dialogai/DialogAiApplication.java
@@ -0,0 +1,13 @@
+package io.martin.dialogai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DialogAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DialogAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/martin/dialogai/DialogAiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.martin.dialogai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DialogAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
